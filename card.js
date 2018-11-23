c1224c2651456eb84deac7f4fcd3965e95de53a7
Kimchunyong <lovemy5854@naver.com>
2018-11-23 15:38:55 +0900

    카드게임 구조 생성

---
+++ card.js
@@ -0,0 +1,13 @@
+var row = 4;
+var col = 3;
+
+function cardCreate(row, col) {
+    for (var i = 0; i < col; i++) {
+        for (var j = 0; j < row; j++) {
+            var card = document.createElement('div');
+            var card = document.createElement('div');
+            var cardFront = document.createElement('div');
+            var cardBack = document.createElement('div');
+        }
+    }
+}
